8f83d43e828dd858fe1eb707c3242202e9076c1b
Kaciras <Kaciras@protonmail.com>
2021-12-30 16:17:22 +0800

    improve item hashing

--- guguzhen/helper.py
+++ guguzhen/helper.py
@@ -11,22 +11,32 @@ _color_map = (
 	Fore.LIGHTBLUE_EX,
 	Fore.LIGHTGREEN_EX,
 	Fore.LIGHTYELLOW_EX,
-	Fore.RED
+	Fore.LIGHTRED_EX,
 )
 
 
 def _do_print_item(item):
 	c = _color_map[item.grade]
 	h = item_hash(item)
-	e = ", ".join(map(str, item.attrs))
+	e = " ".join(map(str, item.attrs))
 
 	if isinstance(item, Amulet):
 		print(f"{h} {c}+{item.enhancement} {item.name}{Fore.RESET} {e}")
 	else:
-		print(f"{h} {c}Lv.{item.level} {item.name}{Fore.RESET} ({e})")
+		m = Fore.RED + " [+神秘属性]" + Fore.RESET if item.mystery else ""
+		print(f"{h} {c}Lv.{item.level} {item.name}{Fore.RESET} {e}{m}")
 
 
 def item_hash(item: Item):
+	"""
+	根据物品的属性生成 hash 值，可用于标识该物品。
+
+	因为 Python 默认启用了 hash 随机化，因此 hash(obj) 的结果每次运行都不同，
+	让用户添加 PYTHONHASHSEED 变量也不方便，于是就自己实现了。
+
+	:param item: 装备或护身符
+	:return: 8 位 base64 hash 值
+	"""
 	m = sha256()
 	m.update(item.grade.to_bytes(1, "big"))
 	m.update(item.name.encode())
@@ -34,11 +44,15 @@ def item_hash(item: Item):
 	if isinstance(item, Amulet):
 		m.update(item.enhancement.to_bytes(1, "big"))
 		for attr in item.attrs:
-			m.update(str(attr).encode())
+			m.update(attr.type.encode())
+			m.update(attr.value.to_bytes(2, "big"))
+			m.update(attr.unit.encode())
 	else:
 		m.update(item.level.to_bytes(2, "big"))
 		for attr in item.attrs:
-			m.update(str(attr).encode())
+			m.update(attr.type.encode())
+			m.update(attr.value.to_bytes(2, "big"))
+			m.update(attr.ratio.to_bytes(2, "big"))
 		if item.mystery:
 			m.update(item.mystery.encode())
 
@@ -48,9 +62,15 @@ def item_hash(item: Item):
 
 def print_equipments(api: GuGuZhen):
 	equip = api.character.get_info()
-	items = api.character.get_repository()
+	items = api.items.get_repository()
 
-	print(f"\n[背包 (容量 = {items.size})]")
+	print(f"\n[正在使用的]")
+	_do_print_item(equip.weapon)
+	_do_print_item(equip.bracelet)
+	_do_print_item(equip.armor)
+	_do_print_item(equip.accessory)
+
+	print(f"\n[背包 (容量={items.size})]")
 	for item in items.backpacks.values():
 		_do_print_item(item)
 
@@ -63,18 +83,7 @@ def print_cards(api: GuGuZhen):
 	print("\n[角色卡片]")
 
 	for card in api.character.get_cards():
-		parts = [
-			str(card.id),
-			f"Lv.{card.level}/{card.lv_max}",
-			str(card.role),
-			f"{card.skills}技能位",
-			f"{card.quality}%品质"
-		]
-
 		color = Fore.RESET
 		if card.in_use:
 			color = Fore.BLUE
-			parts.append("[使用中]")
-
-		line = " ".join(parts)
-		print(color + line + Fore.RESET)
+		print(f"{color}{card}{Fore.RESET}")
