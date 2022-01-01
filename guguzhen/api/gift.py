b5f1a833c5f9108b06f2f806d88f511628b6b4f3
Kaciras <Kaciras@protonmail.com>
2022-01-01 10:50:58 +0800

    rename gift APIs & fetch base value

--- guguzhen/api/gift.py
+++ guguzhen/api/gift.py
@@ -4,15 +4,17 @@ from typing import Literal
 
 from .base import FYGClient, ReadType, LimitReachedError, ClickType
 
-_gp = re.compile(r"今日奖池中共有 (\d+)贝壳 \+ (\d+)星沙 \+ (\d+)装备 \+ (\d+)卡片 \+ ([0-9.]+)%光环点数")
+_gp = re.compile(r"(\d+)贝壳[\s+]+(\d+)星沙[\s+]+(\d+)件?装备[\s+]+(\d+)张?卡片[\s+]+([0-9.]+)%光环点数")
+
 _cp = re.compile(r"\s*([^+]+)\+([0-9.]+)\*(\d+)%")
+
 _gxp = re.compile(r"消耗 (\d+) 星沙", re.MULTILINE)
 
 GiftType = Literal["贝壳", "星沙", "装备", "卡片", "光环"]
 
 
 @dataclass(eq=False, slots=True)
-class CardInfo:
+class Gift:
 	type: GiftType
 	base: float
 	ratio: float
@@ -28,16 +30,30 @@ class GiftApi:
 		self.api = api
 
 	def get_pool(self):
+		"""
+		获取今日的奖池数据,包含总数和我的基本数值两项。
+
+		:return: (总共, 基本数值) 二元组
+		"""
 		html = self.api.get_page("/fyg_gift.php")
 		el = html.xpath('//*[@id="giftinfo"]/div/h2')
-		match = _gp.match(el[0].text)
+		total = _gp.search(el[0].text)
 
-		return {
-			"贝壳": int(match.group(1)),
-			"星沙": int(match.group(2)),
-			"装备": int(match.group(3)),
-			"卡片": int(match.group(4)),
-			"光环": float(match.group(5))
+		el = html.xpath('//*[@id="giftinfo"]/div/h4')
+		base = _gp.search(el[0].text)
+
+		return  {
+			"贝壳": int(total.group(1)),
+			"星沙": int(total.group(2)),
+			"装备": int(total.group(3)),
+			"卡片": int(total.group(4)),
+			"光环": float(total.group(5))
+		}, {
+			"贝壳": int(base.group(1)),
+			"星沙": int(base.group(2)),
+			"装备": int(base.group(3)),
+			"卡片": int(base.group(4)),
+			"光环": float(base.group(5))
 		}
 
 	def get_gifts(self):
@@ -51,7 +67,7 @@ class GiftApi:
 			match = _cp.match(el.text)
 			if match is None:
 				continue
-			info = CardInfo(match.group(1), float(match.group(2)), float(match.group(3)) / 100)
+			info = Gift(match.group(1), float(match.group(2)), float(match.group(3)) / 100)
 			cards[index] = info
 
 		return cards
--- guguzhen/strategy/gift.py
+++ guguzhen/strategy/gift.py
@@ -3,14 +3,16 @@ from copy import copy
 from dataclasses import dataclass
 from typing import List
 
+from ..api import GuGuZhen
+
 
 @dataclass(eq=False, slots=True)
 class GiftSandRule:
 	"""额外翻卡规则，当奖池中剩余的奖励大于指定值时使用星沙追加翻卡"""
 
-	type: str  # 奖励类型
-	value: float  # 奖池剩余大于该值时尝试翻卡
-	limit: int  # 如果需要的星沙大于该值则不翻
+	type: str		# 奖励类型
+	value: float    # 奖池剩余大于该值时尝试翻卡
+	limit: int		# 如果需要的星沙大于该值则不翻
 
 
 class GetGift:
@@ -18,7 +20,7 @@ class GetGift:
 	def __init__(self, sand_usage: List[GiftSandRule] = ()):
 		self.sand_usage = sand_usage
 
-	def run(self, api):
+	def run(self, api: GuGuZhen):
 		pool = _GiftContext(api)
 
 		prev = count = len(pool.opened)
@@ -36,7 +38,7 @@ class GetGift:
 
 			count = len(pool.opened)
 
-		logging.info(f"好运奖励 - 翻了{prev - count}张卡")
+		logging.info(f"好运奖励 - 翻了{count - prev}张卡")
 
 	def _handle_rules(self, pool, index):
 		for rule in self.sand_usage:
@@ -49,12 +51,12 @@ class GetGift:
 
 class _GiftContext:
 
-	def __init__(self, api):
+	def __init__(self, api: GuGuZhen):
 		self.api = api
 		self.remaining = None
 		self.opened = None
 
-		self.total = api.get_gift_pool()
+		self.total = api.gift.get_pool()[0]
 		self._refresh()
 
 	def __getitem__(self, item):
@@ -65,11 +67,11 @@ class _GiftContext:
 		return 2 + (2 ** len(self.opened))
 
 	def open(self, index, use_sand):
-		self.api.open_gift(index, use_sand)
+		self.api.gift.open(index, use_sand)
 		self._refresh()
 
 	def _refresh(self):
-		self.opened = self.api.get_gift_cards()
+		self.opened = self.api.gift.get_gifts()
 		self.remaining = copy(self.total)
 
 		for gift in self.opened.values():
